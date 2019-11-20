edcc56c2998fce847a8234914eb431debd66579f
matzof <matzoffoli@libero.it>
2019-11-20 08:39:21 +0200

    Update train_ac.py

--- train_ac.py
+++ train_ac.py
@@ -41,15 +41,15 @@ for ep in range(episodes):
         # Step the environment and get the rewards and new observations
         previous_state1 = ob1
         (ob1, ob2), (rew1, rew2), done, info = env.step((action1, action2))
-        rew1 += round(length_ep/100)
+        if rew1 == 10:
+            win1 += 1
+        rew1 += round(length_ep/50)
         # Store action's outcome (so that the agent can improve its policy)
         player.agent.store_transition(previous_state1, action_probabilities1, 
                                       action1, rew1, model)
         # store total length of each episode
         length_ep += 1
         # Count the wins
-        if rew1 == 10:
-            win1 += 1
         if render:
             env.render()
         if done:
