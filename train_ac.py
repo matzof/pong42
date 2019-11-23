3fe686362b099f67715a79176e471e246f86a37b
matzof <matzoffoli@libero.it>
2019-11-23 19:45:27 +0200

    Update train_ac.py

--- train_ac.py
+++ train_ac.py
@@ -41,9 +41,7 @@ for ep in range(episodes):
         # adjust reward for training purpose
         if rew1 == 10:
             win1 += 1
-        if rew1 == -10:
-            rew1 = 0
-        rew1 += round(length_ep/4)
+        rew1 += round(length_ep/40)
         
         # Store action's outcome (so that the agent can improve its policy)
 #        player.agent.store_transition(previous_state1, action_probabilities1, 
