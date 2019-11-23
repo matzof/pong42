0311b7e12f9cd8be7e4a234355b437debfbb35ed
matzof <matzoffoli@libero.it>
2019-11-23 22:15:25 +0200

    Update train_ac.py

--- train_ac.py
+++ train_ac.py
@@ -54,14 +54,13 @@ for ep in range(episodes):
         if render:
             env.render()
     
+    state = extract_state_cheating(env, player_id)
+    player.agent.episode_finished(ep, state)
     length_history.append(length_ep)
     observation = env.reset()
     print("episode {} over. Length ep: {}. Mean Length: {:.1f}. Winrate: {:.3f}. Reward: {}".format(ep,
                length_ep, sum(length_history[len(length_history)-1000:])/1000, 
                 win1 / (ep + 1), rew1))
-            
-    state = extract_state_cheating(env, player_id)
-    player.agent.episode_finished(ep, state)
         
             
 #    plot_rewards(length_history)
