1ca776696df30df77f9af3891c1b7e021eba6780
Plan-T42 <shoegaze.kida@gmail.com>
2019-12-04 02:04:31 +0200

    experiment on new_model: add 3 CNN layers, action_space = 3, redifine advantage

---
+++ new_model3.py
@@ -0,0 +1,101 @@
+# add 3 CNN layers, no action convert -> action_space = 3
+# redifine advantage = rewards - values
+
+import numpy as np
+import torch
+from torch import nn
+from torch.nn import functional as F
+from PIL import Image
+import random
+
+class Policy(nn.Module):
+    def __init__(self, action_space = 3, hidden = 64):
+        super().__init__()
+        self.train_device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.gamma = 0.99
+        self.eps_clip = 0.1
+
+        self.conv1 = nn.Conv2d(2, 32, 8, 4)
+        self.conv2 = nn.Conv2d(32, 64, 4, 2)
+        self.conv3 = nn.Conv2d(64, 64, 3, 1)
+        self.reshaped_size = 64*9*9
+        self.fc1 = nn.Linear(self.reshaped_size, hidden)
+        self.fc2_action = nn.Linear(hidden, action_space)
+        self.fc2_value = nn.Linear(hidden, 1)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = x.reshape(-1, self.reshaped_size)
+        x = F.relu(self.fc1(x))
+        logits = self.fc2_action(x)
+        values = self.fc2_value(x)
+        
+        return logits, values
+
+    def state_to_tensor_cnn(self, obs):
+        obs = obs[::2, ::2].mean(axis=-1) # grayscale and downsample
+        obs[obs < 50] = 0 # set background as 0
+        obs[obs != 0] = 1 # set paddles and ball as 1  
+        obs = np.reshape(obs, (1, obs.shape[0], obs.shape[1]))    
+        
+        return obs
+
+    def pre_process_cnn(self, obs, prev_obs):
+        if prev_obs is None:
+            prev_obs = obs
+        obs = self.state_to_tensor_cnn(obs)
+        prev_obs = self.state_to_tensor_cnn(prev_obs)  
+        stack_ob = np.concatenate((prev_obs, obs), axis=0)
+        stack_ob = torch.from_numpy(stack_ob).unsqueeze(0).float().to(self.train_device)
+        
+        return stack_ob
+
+    def get_action(self, d_obs, action=None, action_prob=None, advantage=None, deterministic=False):
+        if action is None:
+            with torch.no_grad():
+                logits, _ = self.forward(d_obs)
+                c = torch.distributions.Categorical(logits=logits)
+                action = int(c.sample().cpu().numpy()[0])
+                action_prob = float(c.probs[0, action].detach().cpu().numpy())
+
+                return action, action_prob
+        '''
+        # policy gradient (REINFORCE)
+        logits = self.layers(d_obs)
+        loss = F.cross_entropy(logits, action, reduction='none') * advantage
+        return loss.mean()
+        '''
+        
+    def PPO_update(self, d_obs, action=None, action_prob=None, rewards=None, deterministic=False):
+        # PPO
+        vs = np.array([[1., 0., 0.], [0., 1., 0.], [0., 0., 1.]])
+        ts = torch.FloatTensor(vs[action.cpu().numpy()])
+        
+        logits, values = self.forward(d_obs)
+        ratios = torch.sum(F.softmax(logits, dim=1) * ts, dim=1) / action_prob
+        advantages = rewards - values.detach()
+        loss1 = ratios * advantages
+        loss2 = torch.clamp(ratios, 1-self.eps_clip, 1+self.eps_clip) * advantages
+        loss = -torch.min(loss1, loss2)
+        loss = torch.mean(loss)
+
+        return loss
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ new_train3.py
@@ -0,0 +1,102 @@
+
+import gym
+import numpy as np
+import random
+import torch
+from torch import nn
+from new_model3 import Policy
+from wimblepong import wimblepong
+from wimblepong.simple_ai import SimpleAi
+
+env = gym.make("WimblepongVisualMultiplayer-v0")
+
+env.reset()
+
+# Define the player IDs for both SimpleAI agents
+player_id = 1
+opponent_id = 3 - player_id
+opponent = SimpleAi(env, opponent_id)
+policy = Policy()
+
+opt = torch.optim.Adam(policy.parameters(), lr=1e-3)
+
+reward_sum_running_avg = None
+for it in range(100000):
+    d_obs_history, action_history, action_prob_history, reward_history = [], [], [], []
+    for ep in range(200):
+        (ob1, ob2), prev_obs = env.reset(), None
+        for t in range(190000):
+            #env.render()
+
+            d_obs = policy.pre_process_cnn(ob1, prev_obs)
+            with torch.no_grad():
+                action, action_prob = policy.get_action(d_obs)
+            
+            prev_obs = ob1
+            action1 = action
+            action2 = opponent.get_action()
+            (ob1, ob2), (rew1, rew2), done, info = env.step((action1, action2))
+            
+            d_obs_history.append(d_obs)
+            action_history.append(action)
+            action_prob_history.append(action_prob)
+            reward_history.append(rew1)
+
+            if done:
+                reward_sum = sum(reward_history[-t:])
+                reward_sum_running_avg = 0.99*reward_sum_running_avg + 0.01*reward_sum if reward_sum_running_avg else reward_sum
+                print('Iteration %d, Episode %d (%d timesteps) - last_action: %d, last_action_prob: %.2f, reward_sum: %.2f, running_avg: %.2f' % (it, ep, t, action, action_prob, reward_sum, reward_sum_running_avg))
+                #print(action_history[-5:])
+                break
+    
+    # compute advantage
+    R = 0
+    discounted_rewards = []
+
+    for r in reward_history[::-1]:
+        if r != 0: R = 0 # scored/lost a point in pong, so reset reward sum
+        R = r + policy.gamma * R
+        discounted_rewards.insert(0, R)
+
+    #print(discounted_rewards[:5])
+
+    discounted_rewards = torch.FloatTensor(discounted_rewards)
+    discounted_rewards = (discounted_rewards - discounted_rewards.mean()) / discounted_rewards.std()
+    
+    # update policy
+    for _ in range(5):
+        n_batch = round(len(action_history)*0.7)
+        idxs = random.sample(range(len(action_history)), n_batch)
+        d_obs_batch = torch.cat([d_obs_history[idx] for idx in idxs], 0)
+        action_batch = torch.LongTensor([action_history[idx] for idx in idxs])
+        action_prob_batch = torch.FloatTensor([action_prob_history[idx] for idx in idxs])
+        rewards_batch = torch.FloatTensor([discounted_rewards[idx] for idx in idxs])
+        #advantage_batch = (advantage_batch - advantage_batch.mean()) / advantage_batch.std()
+              
+        opt.zero_grad()
+        loss = policy.PPO_update(d_obs_batch, action_batch, action_prob_batch, rewards_batch)
+        loss.backward()
+        opt.step()
+    
+        print('Iteration %d -- Loss: %.3f' % (it, loss))
+    if it % 5 == 0:
+        torch.save(policy.state_dict(), 'params.ckpt')
+
+env.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
