820e0bebb121de1deda3d3727f2ab9dfbc09c9f8
matzof <matzoffoli@libero.it>
2019-12-04 19:46:32 +0200

    Update working_train_PPO.py

--- working_train_PPO.py
+++ working_train_PPO.py
@@ -22,7 +22,6 @@ player = Agent42(env, player_id)
 env.set_names(player.get_name(), opponent.get_name())
 
 (ob1, ob2), (rew1, rew2), done, info = env.step((2, 2))
-timesteps = 0
 win1 = 0
 culmulative_reward = 0
 length_history = []
@@ -51,17 +50,10 @@ for it in range(num_iterations):
     
             # store total length of each episode
             length_ep += 1
-            timesteps += 1
     
             # Count the wins
             if render:
-                env.render()        
-            
-            # PPO Update
-            if timesteps % horizon == 0:
-                print("Updating ---------------------------------------------")
-                player.PPO_update() 
-           
+                env.render()                   
     
         # when done:
         win_history.append(1 if rew1==10 else 0)
@@ -76,6 +68,9 @@ for it in range(num_iterations):
               "Mean Length: {:.1f}".format(sum(length_history)/len(length_history)),
               "Winrate: {:.1f}%".format(100*sum(win_history)/len(win_history)))
         length_ep = 0
+    # PPO Update
+    print("Updating ---------------------------------------------")
+    player.PPO_update() 
     
     plt.figure(figsize=(20.0, 10.0))
     plt.xlabel("Number of Iterations (1 iteration is 200 episodes)")
