659a45b86ab1f25402177e8bb4b51ac4b1241766
matzof <matzoffoli@libero.it>
2019-12-04 19:35:18 +0200

    Plot Added

---
+++ working_train_PPO.py
@@ -0,0 +1,97 @@
+import gym
+
+from new_AI42_PPO_fc_logits import Agent42
+from wimblepong.simple_ai import SimpleAi
+import matplotlib.pyplot as plt
+#%%
+env = gym.make("WimblepongVisualMultiplayer-v0")
+#%%
+# Parameters
+render = False
+num_iterations = 100000
+horizon = 9000 # TODO: adapt the PPO Update frequency
+
+# Define the player IDs for both SimpleAI agents
+player_id = 1
+opponent_id = 3 - player_id
+opponent = SimpleAi(env, opponent_id)
+player = Agent42(env, player_id)
+
+# Set the names for both SimpleAIs
+env.set_names(player.get_name(), opponent.get_name())
+
+(ob1, ob2), (rew1, rew2), done, info = env.step((2, 2))
+timesteps = 0
+win1 = 0
+culmulative_reward = 0
+length_history = []
+win_history = []
+
+for it in range(num_iterations):
+    for ep in range(200):
+        done = False
+        length_ep = 0
+    
+        # Reset the environment and observe the initial state
+        ob1, ob2 = env.reset()
+    
+        # Loop until the episode is over
+        while not done:
+    
+            # Get the actions from both AIs
+            action1 = player.get_action(ob1)
+            action2 = opponent.get_action()
+    
+            # Step the environment and get the rewards and new observations
+            (ob1, ob2), (rew1, rew2), done, info = env.step((action1, action2))
+    
+            # Store the results (reward and done) of the step performed
+            player.store_result(rew1, done)
+    
+            # store total length of each episode
+            length_ep += 1
+            timesteps += 1
+    
+            # Count the wins
+            if render:
+                env.render()        
+            
+            # PPO Update
+            if timesteps % horizon == 0:
+                print("Updating ---------------------------------------------")
+                player.PPO_update() 
+           
+    
+        # when done:
+        win_history.append(1 if rew1==10 else 0)
+        length_history.append(length_ep)
+        
+        if len(win_history) == 500:
+            length_history.pop(0)
+            win_history.pop(0)
+        
+        print("Iter:", it, "Ep:", ep, "Length ep:", length_ep, 
+              "Reward: {:.0f}".format((rew1+10)/20),
+              "Mean Length: {:.1f}".format(sum(length_history)/len(length_history)),
+              "Winrate: {:.1f}%".format(100*sum(win_history)/len(win_history)))
+        length_ep = 0
+    
+    plt.figure(figsize=(20.0, 10.0))
+    plt.xlabel("Number of Iterations (1 iteration is 200 episodes)")
+    plt.plot(length_history)
+    plt.legend("1", "2")
+    plt.grid()
+    plt.savefig("performance_plot.png")
+    plt.close()
+
+
+
+
+
+
+
+
+
+
+
+
