9652418238e74d92ccdd0ed168c3b496c9087d8f
matzof <matzoffoli@libero.it>
2019-12-05 16:03:09 +0200

    Update working_train_PPO.py

--- working_train_PPO.py
+++ working_train_PPO.py
@@ -24,7 +24,6 @@ env.set_names(player.get_name(), opponent.get_name())
 (ob1, ob2), (rew1, rew2), done, info = env.step((2, 2))
 win1 = 0
 culmulative_reward = 0
-plot_count = 0
 length_history = []
 win_history = []
 mean_winrate_history = []
@@ -62,7 +61,7 @@ for it in range(num_iterations):
         win_history.append(1 if rew1==10 else 0)
         length_history.append(length_ep)
         
-        if len(win_history) == 500:
+        if len(win_history) == 200:
             length_history.pop(0)
             win_history.pop(0)
         
@@ -71,25 +70,23 @@ for it in range(num_iterations):
               "Mean Length: {:.1f}".format(sum(length_history)/len(length_history)),
               "Winrate: {:.1f}%".format(100*sum(win_history)/len(win_history)))
         length_ep = 0
-        
-        if ep % 20 == 0:
-            plot_count += 1
-            # Plot Mean Reward History
-            mean_winrate_history.append(100*sum(win_history)/len(win_history))
-            mean_length_history.append(0.333*sum(length_history)/len(length_history))
-            plt.figure(figsize=(20.0, 10.0))
-            plt.xlabel("Number of Iterations*10", fontsize=30)
-            plt.plot(mean_winrate_history)
-            plt.plot(mean_length_history)
-            plt.legend(["Mean Winrate", "Mean Length of Episodes (divided by 3)"], fontsize ='xx-large')
-            plt.grid()
-            plt.savefig("plots/" + str(plot_count) + "training_performance_plot.png")
-            plt.close()
-    
+            
     # Saving Model
     if it % 1 == 0:
         print("Saving -----------------------------------------------")
         player.store_model(it)
+
+    # Plot Mean Reward History
+    mean_winrate_history.append(100*sum(win_history)/len(win_history))
+    mean_length_history.append(0.2*sum(length_history)/len(length_history))
+    plt.figure(figsize=(20.0, 10.0))
+    plt.xlabel("Number of Iterations", fontsize=30)
+    plt.plot(mean_winrate_history)
+    plt.plot(mean_length_history)
+    plt.legend(["Mean Winrate", "Mean Length of Episodes (divided by 5)"], fontsize ='xx-large')
+    plt.grid()
+    plt.savefig("plots/" + str(plot_count) + "training_performance_plot.png")
+    plt.close()
     
     # PPO Update
     print("Updating ---------------------------------------------")
