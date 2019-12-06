1188cee5be0ce5588ad1d78f66f7cc3f9e284365
matzof <matzoffoli@libero.it>
2019-12-06 13:40:28 +0200

    Fixes

--- resume_train_PPO_4_with_agent.py
+++ resume_train_PPO_4_with_agent.py
@@ -1,7 +1,7 @@
 import gym
 
 from AI42_PPO_4 import Agent42
-from AI42_PPO_6 import Agent
+from AI42_PPO_6 import Agent42 as Agent
 from wimblepong.simple_ai import SimpleAi
 import matplotlib
 matplotlib.use('Agg')
--- resume_train_PPO_6_with_otheragent.py
+++ resume_train_PPO_6_with_otheragent.py
@@ -1,7 +1,7 @@
 import gym
 
 from AI42_PPO_6 import Agent42
-from AI42_PPO_4 import Agent
+from AI42_PPO_4 import Agent42 as Agent
 from some_other_agent import Agent as SomeOtherAgent
 from wimblepong.simple_ai import SimpleAi
 import matplotlib
